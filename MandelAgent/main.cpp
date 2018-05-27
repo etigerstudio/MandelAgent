c73680e6c9279ccada248ca2f0ce976411dbd9ff
ALuier Bondar <etigerstudio@gmail.com>
2018-05-27 17:46:52 +0800

    Implement complex multiply function.

--- MandelAgent/main.cpp
+++ MandelAgent/main.cpp
@@ -10,6 +10,16 @@
 #include <png++/png.hpp>
 #include <mpi.h>
 
+struct complex {
+    double real, imag;
+};
+
+complex multi(complex c1, complex c2){
+    return complex
+    {c1.real * c2.real - c1.imag * c2.imag,
+        c1.real * c2.imag + c1.imag * c2.real};
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
