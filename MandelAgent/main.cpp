08728319dbc9a16d78965ed465b8289164bbb7a6
ALuier Bondar <etigerstudio@gmail.com>
2018-05-28 19:51:51 +0800

    Move const definition to header.

--- MandelAgent/main.cpp
+++ MandelAgent/main.cpp
@@ -13,20 +13,6 @@
 
 using namespace png;
 
-const int RESOLUTION = 400;
-const int ITERATION = 1000;
-const double X_MIN = -2;
-const double X_MAX = 2;
-const double Y_MIN = -2;
-const double Y_MAX = 2;
-const double MIN_X = -2;
-const double MAX_X = 1;
-const double MIN_Y = -1.5;
-const double MAX_Y = 1.5;
-
-const rgb_pixel WHITE = rgb_pixel(255, 255, 255);
-const rgb_pixel BLACK = rgb_pixel(0, 0, 0);
-
 bool isMandel(complex c){
     int count = 0;
     complex z = c;
--- MandelAgent/main.h
+++ MandelAgent/main.h
@@ -11,6 +11,19 @@
 
 using namespace png;
 
+const int RESOLUTION = 400;
+const int ITERATION = 1000;
+const double MIN_X = -2;
+const double MAX_X = 1;
+const double MIN_Y = -1.5;
+const double MAX_Y = 1.5;
+
+const int COLS = 2;
+const int ROWS = 2;
+
+const rgb_pixel WHITE = rgb_pixel(255, 255, 255);
+const rgb_pixel BLACK = rgb_pixel(0, 0, 0);
+
 class complex {
 public:
     double real, imag;
